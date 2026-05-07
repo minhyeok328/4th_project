ad9bd46154cc0da14fb3ede5ae9ac62a6b8ae301
Ocean-2930 <oceanblue1590@gmail.com>
2026-05-07 12:34:46 +0900

    feat: 사용자 로그인 로직 추가
    Refs: #14

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def accountpage(request):
@@ -8,7 +9,30 @@ def accountpage(request):
     )
 
 def loginpage(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            request.session.pop("login_fail", None)
+            return redirect("mainpage:mainpage")
+
+        request.session["login_fail"] = True
+        return redirect("accounts:loginpage")
+
+    login_fail = request.session.pop("login_fail", False)
+
     return render(
         request,
-        "loginpage.html"
+        "loginpage.html",
+        {
+            "login_fail": login_fail
+        }
     )
